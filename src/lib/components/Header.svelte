e20fad9087f68d42516a8f98c288bf74828aa748
@kaspar <team@crewnew.com>
2022-11-07 14:59:08 +0100

    Underline active menu element

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+<nav class="flex justify-center gap-5">
+    <a href="/" class:nav-active-route={$page.url.pathname === '/'}>Home</a>
+    <a href="/about" class:nav-active-route={$page.url.pathname === '/about'}>About</a>
+    <a href="/sign-up" class:nav-active-route={$page.url.pathname === '/sign-up'}>Sign up</a>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-    import "../app.css";
+    import Header from "$lib/components/Header.svelte";
+	import '../app.css';
 </script>
+
+<div style="bg-slate-100 h-100">
+	<Header/>
+</div>
 
 <slot />
