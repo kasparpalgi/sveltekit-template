c281638642f6d952c107f6f2d5bf0bf98f109ab7
@kaspar <team@crewnew.com>
2022-11-07 13:09:49 +0100

    Install Tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,14 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter()
-	}
+	},
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+	],
 };
 
 export default config;
