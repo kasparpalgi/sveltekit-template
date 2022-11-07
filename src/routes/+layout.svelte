e412873a6f4d43264bd422ee9f1da4be7009b8da
@kaspar <team@crewnew.com>
2022-11-07 15:03:27 +0100

    Align main content in the center

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-    import Header from "$lib/components/Header.svelte";
+	import Header from '$lib/components/Header.svelte';
 	import '../app.css';
 </script>
 
-<div style="bg-slate-100 h-100">
-	<Header/>
+<div style="bg-slate-100 h-screen">
+	<Header />
 </div>
 
-<slot />
+<div class="max-w-max mx-auto">
+	<slot />
+</div>
