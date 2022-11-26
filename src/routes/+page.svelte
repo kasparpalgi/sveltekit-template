05f3713806b0373e4c011af1a3fd3ef0e706ec8e
@kaspar <team@crewnew.com>
2022-11-26 17:18:36 +0100

    Start features part

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,3 +25,34 @@
         </div>
 	</div>
 </section>
+
+<section id="features">
+    <div class="container flex flex-col px-4 mx-auto mt-10 space-y-12 md:space-y-0 md:flex-row">
+        <!-- What's different -->
+        <div class="flex flex-col space-y-12 md:w-1/1">
+            <h2 class="max-w-md text-4xl font-bold text-center md:text-left">
+                What's different about Klarity?
+            </h2>
+            <p class="max-w-sm text-center text-darkGrayishBlue md:text-left">
+                Other time trackers that claim to be automated, will just give you a timeline where you see what app/program did you use during certain timeframe but it won't tell you what project/task did you work on so you will need to manually drag'n'drop timeframes. Bit better than manual time tracking but not ideal like Klarity.
+            </p>
+        </div>
+            <!-- Numbered list -->
+            <div class="flex flex-col space-y-8 md:w-1/2">
+                <!-- Numbered item -->
+                <div class="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
+                    <!-- Heading -->
+                    <div class="rounded-l-full bg-brightRedSupLight md:bg-transparent">
+                        <div class="flex items-center space-x-2">
+                            <div class="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed">
+                                01
+                            </div>
+                            <h3 class="text-base font-bold md:mb-4">
+                                Gain Klarity on where the time is spent
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</section>
