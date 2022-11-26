fa852b217b9b7cb505631e331c335a86ed48aaa1
@kaspar <team@crewnew.com>
2022-11-26 12:16:10 +0100

    Go to Tailwind course CSS

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { page } from '$app/stores';
 </script>
-<nav class="flex justify-center gap-5">
+<nav class="relative container mx-auto p-6">
     <a href="/" class:nav-active-route={$page.url.pathname === '/'}>Home</a>
     <a href="/about" class:nav-active-route={$page.url.pathname === '/about'}>About</a>
     <a href="/sign-up" class:nav-active-route={$page.url.pathname === '/sign-up'}>Sign up</a>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,9 +10,14 @@ module.exports = {
     },
     extend: {
       colors: {
-        cnPurple: '#42375b',
-        cnGreen: '#00d857',
-        cnRed: '#d94167'
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)'
       }
     }
   },
