1c8cd6fdb41da8bf874cc8e861cf61d871a08e36
@kaspar <team@crewnew.com>
2022-11-26 13:09:01 +0100

    Add template images and colors

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-    import { page } from '$app/stores';
+	import { page } from '$app/stores';
 </script>
+
 <nav class="relative container mx-auto p-6">
-    <a href="/" class:nav-active-route={$page.url.pathname === '/'}>Home</a>
-    <a href="/about" class:nav-active-route={$page.url.pathname === '/about'}>About</a>
-    <a href="/sign-up" class:nav-active-route={$page.url.pathname === '/sign-up'}>Sign up</a>
+	<div class="flex items-center justify-between">
+		<a href="/" class:nav-active-route={$page.url.pathname === '/'}>Home</a>
+		<a href="/about" class:nav-active-route={$page.url.pathname === '/about'}>About</a>
+		<a href="/sign-up" class:nav-active-route={$page.url.pathname === '/sign-up'}>Sign up</a>
+	</div>
 </nav>
