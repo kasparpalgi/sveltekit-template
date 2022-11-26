8de9be97205cb091696e8b8601a2e64d0746b338
@kaspar <team@crewnew.com>
2022-11-26 11:44:00 +0100

    Update tailwind.config.cjs - add custom screen sizes and default colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        cnPurple: '#42375b',
+        cnGreen: '#00d857',
+        cnRed: '#d94167'
+      }
+    }
   },
   plugins: [require("daisyui")]
 };
